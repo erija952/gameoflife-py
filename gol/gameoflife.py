fa73b198ff2f13d942e95f90625a2e9d5c65198b
eral <erik.almqvist@cybercom.com>
2013-10-22 14:30:22 +0200

    Added test for rule1, underpopulation

--- gol/gameoflife.py
+++ gol/gameoflife.py
@@ -2,6 +2,8 @@ from gol.outputformater import outputformater
 import gameoflifeboard
 import platform
 import subprocess
+import numpy as np
+
 class gameoflife(object):
 
     def __init__(self):        
@@ -23,3 +25,26 @@ class gameoflife(object):
         
     def clear(self):
         subprocess.Popen( "cls" if platform.system() == "Windows" else "clear", shell=True)
+
+
+    def update_r1_underpopulation(self, board):    
+#         (rows,cols) = np.shape(self.gol.board)
+#         for x in range(0,rows)
+#             for y in range(0,cols)
+#                 count_neighbours()
+        #Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+        return board
+    
+    def update_r2_survivors(self):
+        #Any live cell with two or three live neighbours lives on to the next generation.
+        pass
+    
+    def update_r3_crowding(self):
+        #Any live cell with more than three live neighbours dies, as if by overcrowding.
+        pass
+        
+    def update_r4_reproduction(self):
+        #Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        pass
+    
+    
--- gol/test/gameoflifetest.py
+++ gol/test/gameoflifetest.py
@@ -1,15 +1,38 @@
 import unittest
 from gol.gameoflife import gameoflife
+import numpy as np
+
+
+
 
 class Test(unittest.TestCase):
 
-
-    def testName(self):
-#         gol = gameoflife()       
-#         gol.update()
-#         self.assertEqual()
+    def boardtest(self, expected_board, new_board):
+        print "Exp"
+        print expected_board
+        print "New"
+        print new_board
+        
+        (exp_rows,exp_cols) = np.shape(new_board)
+        (rows,cols) = np.shape(new_board)
+        assert (exp_rows == rows)
+        assert (exp_cols == cols)    
+    
+        for x in range(0,rows):
+            for y in range(0,cols):
+                self.assertEqual(new_board[x][y], expected_board[x][y], "Board not updated correctly at (" + str(x) + "," + str(y)+")." )
+         
+#    def testRun(self):
+#        gol = gameoflife()
+#        gol.run()
+        
+    def testshouldhandlefirstruleofgol(self):
+        board =         [['1', '0', '0','1'], ['1', '1', '0','0'], ['0', '0', '0','0'],['0', '0', '1','1']]
+        expected_board= [['1', '0', '0','0'], ['1', '1', '0','0'], ['0', '0', '0','0'],['0', '0', '0','0']]
         gol = gameoflife()
-        gol.run()
+        new_board = gol.update_r1_underpopulation(board)
+        self.boardtest(expected_board,new_board)
+        
         
 
 if __name__ == "__main__":
