dd715476ce57fe7c9cea3efcdfa0d83cbea12330
eral <erik.almqvist@cybercom.com>
2013-10-23 13:03:25 +0200

    Added rules for gol, now works

--- gol/gameoflife.py
+++ gol/gameoflife.py
@@ -10,41 +10,63 @@ class gameoflife(object):
         pass
     
     def run(self):
+        golb = gameoflifeboard.gameoflifeboard()
+        golb.createBoard(10, 10)
+        golb.populate()
+        board = golb.board
+        print board
         myinput = ''
         while myinput != 'c':
-            self.update()
+            board = self.update_board(board)
+            of = outputformater(u'\u25FC',u'\u25FD')
+            of.output(board)
             myinput = raw_input(':')
             self.clear()         
-    
-    def update(self):
-        golBoard = gameoflifeboard.gameoflifeboard()
-        golBoard.createBoard(5,5)
-        golBoard.populate()
-        of = outputformater(u'\u25FC',u'\u25FD')
-        of.output(golBoard.board)               
         
     def clear(self):
         subprocess.Popen( "cls" if platform.system() == "Windows" else "clear", shell=True)
-
-
-    def update_r1_underpopulation(self, board):    
-#         (rows,cols) = np.shape(self.gol.board)
-#         for x in range(0,rows)
-#             for y in range(0,cols)
-#                 count_neighbours()
-        #Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-        return board
-    
-    def update_r2_survivors(self):
-        #Any live cell with two or three live neighbours lives on to the next generation.
-        pass
-    
-    def update_r3_crowding(self):
-        #Any live cell with more than three live neighbours dies, as if by overcrowding.
-        pass
         
-    def update_r4_reproduction(self):
-        #Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-        pass
+    def count_neighbours(self, y, x, board):       
+        (max_rows,max_cols) = np.shape(board)
+        #todo: assumes board large than 2*2
+        nr_neighbours = 0       
+        for yi in range(-1,2):           
+            for xi in range(-1,2):
+                curx=x+xi
+                cury=y+yi
+                if not(xi == 0 and yi == 0): #Don't count current coordinate
+                    if curx >= 0 and curx < max_rows:
+                        if cury >= 0 and cury < max_cols:
+                            if board[cury][curx] == '1':
+                                nr_neighbours +=1
+        return nr_neighbours 
     
+    def live_cell_survives_to_next_generation_r1r2r3(self, nr_neighbours):
+        if nr_neighbours == 2 or nr_neighbours == 3:
+            return True
+        else:
+            return False
+        
+    def dead_cell_resurrects_r4(self, nr_neighbours):
+        if nr_neighbours == 3:
+            return True
+        else:
+            return False
     
+    def update_board(self,board):
+        (rows,cols) = np.shape(board)
+        new_board = []
+        for x in range(0,rows):  # @UnusedVariable
+            new_board.append(["0"] * cols)        
+                    
+        for y in range(0,cols):
+            for x in range(0,rows):
+                nr_neighbours = self.count_neighbours(y, x, board)
+                               
+                if board[y][x] == '1':
+                    if self.live_cell_survives_to_next_generation_r1r2r3(nr_neighbours):
+                        new_board[y][x] = '1'
+                else:
+                    if self.dead_cell_resurrects_r4(nr_neighbours):
+                        new_board[y][x] = '1'
+        return new_board
--- gol/test/gameoflifetest.py
+++ gol/test/gameoflifetest.py
@@ -2,11 +2,17 @@ import unittest
 from gol.gameoflife import gameoflife
 import numpy as np
 
-
-
-
 class Test(unittest.TestCase):
+ 
+    def setUp(self):
+        self.gol = gameoflife()
+        self.board =         [['1', '0', '0','1'], ['1', '1', '0','0'], ['1', '0', '0','0'],['0', '0', '1','1']]
+        self.expected_board= [['1', '1', '0','0'], ['1', '1', '0','0'], ['1', '0', '1','0'],['0', '0', '0','0']]
+        pass
 
+    def tearDown(self):
+        pass
+    
     def boardtest(self, expected_board, new_board):
         print "Exp"
         print expected_board
@@ -20,20 +26,39 @@ class Test(unittest.TestCase):
     
         for x in range(0,rows):
             for y in range(0,cols):
-                self.assertEqual(new_board[x][y], expected_board[x][y], "Board not updated correctly at (" + str(x) + "," + str(y)+")." )
+                self.assertEqual(new_board[y][x], expected_board[y][x], "Board not updated correctly at (" + str(x) + "," + str(y)+")." )
          
-#    def testRun(self):
-#        gol = gameoflife()
-#        gol.run()
-        
-    def testshouldhandlefirstruleofgol(self):
-        board =         [['1', '0', '0','1'], ['1', '1', '0','0'], ['0', '0', '0','0'],['0', '0', '1','1']]
-        expected_board= [['1', '0', '0','0'], ['1', '1', '0','0'], ['0', '0', '0','0'],['0', '0', '0','0']]
+    def testRun(self):
         gol = gameoflife()
-        new_board = gol.update_r1_underpopulation(board)
-        self.boardtest(expected_board,new_board)
+        gol.run()
+
+    def testcountneighbours(self):
+        self.assertEqual(self.gol.count_neighbours(0,0,self.board), 2, "Should give correct number of neighbours")
+        self.assertEqual(self.gol.count_neighbours(1,0,self.board), 3, "Should give correct number of neighbours")
+        self.assertEqual(self.gol.count_neighbours(3,1,self.board), 2, "Should give correct number of neighbours")
+        self.assertEqual(self.gol.count_neighbours(1,1,self.board), 3, "Should give correct number of neighbours")
+        self.assertEqual(self.gol.count_neighbours(3,3,self.board), 1, "Should give correct number of neighbours")
+               
+    def testwheterlivecellshouldsurvivetonextgeneration(self):
+        '''Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+        Any live cell with two or three live neighbours lives on to the next generation.    
+        Any live cell with more than three live neighbours dies, as if by overcrowding.'''
+        self.assertFalse(self.gol.live_cell_survives_to_next_generation_r1r2r3(0), "Live cell with X neighbours should not survive to next generation")
+        self.assertFalse(self.gol.live_cell_survives_to_next_generation_r1r2r3(1), "Live cell with X neighbours should not survive to next generation")
+        self.assertTrue(self.gol.live_cell_survives_to_next_generation_r1r2r3(2), "Live cell with X neighbours should survive to next generation")
+        self.assertTrue(self.gol.live_cell_survives_to_next_generation_r1r2r3(3), "Live cell with X neighbours should survive to next generation")
+        self.assertFalse(self.gol.live_cell_survives_to_next_generation_r1r2r3(4), "Live cell with X neighbours should not survive to next generation")
         
+    def testwheterdeadcellshouldsurvivetonextgeneration(self):
+        '''Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.'''
+        self.assertFalse(self.gol.dead_cell_resurrects_r4(0), "Dead cell with X neighbours should not come to life")
+        self.assertFalse(self.gol.dead_cell_resurrects_r4(1), "Dead cell with X neighbours should not come to life")
+        self.assertTrue(self.gol.dead_cell_resurrects_r4(3), "Dead cell with X neighbours should not come to life")
+        self.assertFalse(self.gol.dead_cell_resurrects_r4(4), "Dead cell with X neighbours should not come to life")
         
+    def testupdateboardaccordingtorules(self):
+        new_board = self.gol.update_board(self.board)       
+        self.boardtest(self.expected_board,new_board)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
